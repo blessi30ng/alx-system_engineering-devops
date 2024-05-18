543fc28e5fd686d35fac7d45e2f5c32a51fb3eda
blessi30ng <blessingchidewu3@gmail.com>
2024-05-18 08:35:39 -0700

    lol

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,14 +4,14 @@ include stdlib
 
 file_line { 'Turn off passwd auth':
 	ensure => present,
-	path => '/etc/ssh/ssh_config',
-	lin => '      PasswordAuthentication no',
+	path   => '/etc/ssh/ssh_config',
+	lin    => '      PasswordAuthentication no',
 	replace => true,
 }
 
 file_line { 'Declar identity file':
 	ensure => present,
-	path => '/etc/ssh/ssh_config',
-	line => '     IdentityFile ~/.ssh/school',
+	path   => '/etc/ssh/ssh_config',
+	line   => '     IdentityFile ~/.ssh/school',
 	replace => true,
 }
