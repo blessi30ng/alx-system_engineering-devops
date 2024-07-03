21846560e535bd897ebe008345b3660745e2cbe7
blessi30ng <blessingchidewu3@gmail.com>
2024-07-03 05:37:39 -0700

    lol

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# using puppet to make to configuration file
+
+file { 'etc/ssh/ssh_config':
+	ensure => present,
+
+
+content =>"
+
+	#SSH client configuration
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+	",
+
+}
