e0a40127395a2a29b7f22f926b8006f4d86bdc18
blessi30ng <blessingchidewu3@gmail.com>
2024-07-03 04:07:57 -0700

    lol

---
+++ 0x0C-web_server1/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,23 @@
+# using puppet to automate work
+
+
+package { 'nginx' :
+  ensuere => installed'
+}
+
+file_line { 'install':
+  ensure => 'present',
+  path => '/etc/nginx/sites-enabled/default',
+  after => 'listen 80 default_server;',
+  line => 'rewrite ^/redirect_me hhtps://www.github.com//relentless30 permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!':,
+}
+
+
+service { 'nginx':
+  ensure => running,
+  require => Package['nginx']'
+}
